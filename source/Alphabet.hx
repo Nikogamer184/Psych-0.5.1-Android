1ddeaabf19afdbb0281fad8963a2fad7abe19d1a
Nikogamer184 <98852651+Nikogamer184@users.noreply.github.com>
2022-07-13 12:58:20 -0400

    Update Alphabet.hx

--- source/Alphabet.hx
+++ source/Alphabet.hx
@@ -330,21 +330,15 @@ class Alphabet extends FlxSpriteGroup
 		}
 	}
 
-	override function update(elapsed:Float)
-	{
-		if (isMenuItem)
-		{
+	override function update(elapsed:Float) {
+		if (isMenuItem) {
 			var scaledY = FlxMath.remapToRange(targetY, 0, 1, 0, 1.3);
 
 			var lerpVal:Float = CoolUtil.boundTo(elapsed * 9.6, 0, 1);
 			y = FlxMath.lerp(y, (scaledY * yMult) + (FlxG.height * 0.48) + yAdd, lerpVal);
-		if (isCentered)
-		{
+		if (isCentered) {
 			screenCenter(X);
-		}
-
-		else
-		{
+		} else {
 			if(forceX != Math.NEGATIVE_INFINITY) {
 				x = forceX;
 			} else {
