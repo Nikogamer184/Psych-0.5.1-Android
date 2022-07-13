4e4fe8202d0b566b8e6d890f0e47df7de3c5304c
Nikogamer184 <98852651+Nikogamer184@users.noreply.github.com>
2022-07-13 13:38:38 -0400

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -107,6 +107,7 @@ class PauseSubState extends MusicBeatSubstate
 		{
 			var songText:Alphabet = new Alphabet(0, (70 * i) + 30, menuItems[i], true, false);
 			songText.isMenuItem = true;
+			songText.isCenteredX = true;
 			songText.targetY = i;
 			grpMenuShit.add(songText);
 		}
@@ -257,6 +258,7 @@ class PauseSubState extends MusicBeatSubstate
 		for (i in 0...menuItems.length) {
 			var item = new Alphabet(0, 70 * i + 30, menuItems[i], true, false);
 			item.isMenuItem = true;
+			item.isCenteredX = true;
 			item.targetY = i;
 			grpMenuShit.add(item);
 		}
